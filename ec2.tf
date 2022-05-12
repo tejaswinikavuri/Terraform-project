0ed8ed0a4643a45e54c40a80ee1c59dd8609aa69
tejaswinikavuri <kavuritejaswini41@gmail.com>
2022-05-12 12:12:47 +0100

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
-  access_key = "************************"
+  access_key = "***********************"
   secret_key = "******************************"            
 }
 
