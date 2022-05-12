c7a6f5ef9b3dcfd0149188ebb831ba9d56c8b36a
tejaswinikavuri <kavuritejaswini41@gmail.com>
2022-05-12 12:06:04 +0100

    commit

--- rds.tf
+++ rds.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t2.micro"
   db_name              = "mydatabase"
   username             = "mydatabase"
-  password             = "**************"
+  password             = "*************"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
