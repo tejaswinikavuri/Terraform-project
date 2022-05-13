9440e0726b3f35b368326e8e56f688ea06eba354
tejaswinikavuri <kavuritejaswini41@gmail.com>
2022-05-13 13:03:18 +0100

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  region  = "us-east-1"
-  access_key = "*************************"
-  secret_key = "*****************************"            
-}
-
-
-
 resource "aws_instance" "my-first-server" {
   ami           = "ami-09d56f8956ab235b3"
   instance_type = "t2.micro"
